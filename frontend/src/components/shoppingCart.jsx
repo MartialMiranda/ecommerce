6a76516a56a3bd98a7549899634769f7a9f5f654
martialm79 <martialm79@gmail.com>
2024-11-25 15:08:17 -0400

    Agregada de nueva funcionalidad

---
+++ frontend/src/components/shoppingCart.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const shoppingCart = () => {
+  return (
+    <View>
+      <Text>shoppingCart</Text>
+    </View>
+  )
+}
+
+export default shoppingCart
---
+++ frontend/src/pages/perfil_usuario.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const perfil_usuario = () => {
+  return (
+    <View>
+      <Text>perfil_usuario</Text>
+    </View>
+  )
+}
+
+export default perfil_usuario
